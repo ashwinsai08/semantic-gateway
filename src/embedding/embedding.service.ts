7a3a995638fcc845746babf5c0a752a63436211d
Ashwin Sai <ashwinsai944@gmail.com>
2026-04-16 23:09:15 +0530

    added proper commentings

--- src/embedding/embedding.service.ts
+++ src/embedding/embedding.service.ts
@@ -4,16 +4,23 @@ import axios from 'axios';
 @Injectable()
 export class EmbeddingService {
 
+  /**
+   * Service to embed i.e to change the user's string input to embed vectors
+   * Uses a LLM to do (TODO: Change to a service in future)
+   */
   async embed(text: string): Promise<number[]> {
     try {
+
+      // Call made to LLM to nomic-embed-text (to convert to vectors)
       const response = await axios.post(process.env.OLLAMA_URL as string, {
         model: 'nomic-embed-text', // local model
         prompt: text,
       },
-        {
-          timeout: 5000
-        });
+      {
+        timeout: 5000
+      });
 
+      // Return the embeded resoponse
       return response.data.embedding;
     } catch (error) {
       console.error('Embedding error:', error?.response?.data || error.message);
@@ -22,6 +29,7 @@ export class EmbeddingService {
   }
 
   // For multiple inputs
+  // TODO
   async embedBatch(texts: string[]): Promise<number[][]> {
     try {
       const response = await axios.post(
