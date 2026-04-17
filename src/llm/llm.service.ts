42a02b990caaadcca6b3feed83820c4df8e39d99
Ashwin Sai <ashwinsai944@gmail.com>
2026-04-17 22:47:50 +0530

    llm file changes

--- src/llm/llm.service.ts
+++ src/llm/llm.service.ts
@@ -3,11 +3,18 @@ import OpenAI from 'openai';
 
 @Injectable()
 export class LlmService {
+  
+  //  Client URL and API KEY (GROKQ used here)
   private client = new OpenAI({
     apiKey: process.env.GROQ_API_KEY,
     baseURL: 'https://api.groq.com/openai/v1',
   });
 
+  /**
+   * Service to call the LLM (LLAMA used here)
+   * @param prompt - String form the user
+   * @returns - The response from LLM
+   */
   async generate(prompt: string): Promise<any> {
     const response = await this.client.chat.completions.create({
       model: 'llama-3.1-8b-instant',
