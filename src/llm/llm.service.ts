6508d408f45738547196c670b412707176342482
Ashwin Sai <ashwinsai944@gmail.com>
2026-04-17 22:56:03 +0530

    logger added in llm service

--- src/llm/llm.service.ts
+++ src/llm/llm.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import OpenAI from 'openai';
+import { Logger } from 'winston';
 
 @Injectable()
 export class LlmService {
-  
+
   //  Client URL and API KEY (GROKQ used here)
   private client = new OpenAI({
     apiKey: process.env.GROQ_API_KEY,
     baseURL: 'https://api.groq.com/openai/v1',
   });
+  private readonly logger = new Logger
 
   /**
    * Service to call the LLM (LLAMA used here)
@@ -16,6 +18,7 @@ export class LlmService {
    * @returns - The response from LLM
    */
   async generate(prompt: string): Promise<any> {
+    this.logger.info('[LlmService:generate]: Api called to generate the request from LLM')
     const response = await this.client.chat.completions.create({
       model: 'llama-3.1-8b-instant',
       messages: [
